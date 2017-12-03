b8f629c25edeca40d1c6831b1f6e25577fae8f0d
SkyBlade1978 <johnbraham1978@outlook.com>
2017-12-03 21:06:48 +0000

    Fixed creative tab

--- src/main/java/com/mcmoddev/mineralogy/blocks/RockSlab.java
+++ src/main/java/com/mcmoddev/mineralogy/blocks/RockSlab.java
@@ -60,15 +60,15 @@ public class RockSlab extends net.minecraft.block.Block {
 		}
 	}
 
-	public RockSlab(float hardness, float blastResistance, int toolHardnessLevel, SoundType sound) {
+	public RockSlab(float hardness, float blastResistance, int toolHardnessLevel, SoundType sound, CreativeTabs tab) {
 		super(Material.ROCK);
 		this.setHardness((float) hardness); // dirt is 0.5, grass is 0.6, stone is 1.5,iron ore is 3, obsidian is 50
 		this.setResistance((float) blastResistance); // dirt is 0, iron ore is 5, stone is 10, obsidian is 2000
 		this.setSoundType(sound); // sound for stone
 		this.setHarvestLevel("pickaxe", toolHardnessLevel);
-		this.setCreativeTab(CreativeTabs.DECORATIONS);
 		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.UP));
 		this.useNeighborBrightness = true;
+		this.setCreativeTab(tab);
 	}
 
 	@Deprecated

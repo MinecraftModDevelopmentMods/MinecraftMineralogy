2e809d6b04ddd4cf53976ff548ffe5c5bae3fd0a
SkyBlade1978 <johnbraham1978@outlook.com>
2018-02-03 22:42:43 +0000

    langs complete for english

--- src/main/java/com/mcmoddev/mineralogy/blocks/RockWall.java
+++ src/main/java/com/mcmoddev/mineralogy/blocks/RockWall.java
@@ -2,9 +2,12 @@ package com.mcmoddev.mineralogy.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
  
 /**
 *
@@ -24,5 +27,10 @@ public class RockWall extends net.minecraft.block.BlockWall {
 	 @Override
 	 public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
 	 	list.add(new ItemStack(this, 1, net.minecraft.block.BlockWall.EnumType.NORMAL.getMetadata()));
+	 }
+	 
+	 @Override
+	 public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos) {
+	         return true;
 	 }
  }

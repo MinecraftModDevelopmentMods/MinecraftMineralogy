b6bab64f705fd5c35ad4dff04b66dca93d28302d
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-02-28 15:23:46 -0800

    Should be ready for release.

--- src/main/java/cyano/mineralogy/Mineralogy.java
+++ src/main/java/cyano/mineralogy/Mineralogy.java
@@ -52,10 +52,10 @@ public class Mineralogy {
 			return true;
 		};
 	};
-	
+
 	public static final String MODID = "mineralogy";
     public static final String NAME ="Mineralogy";
-    public static final String VERSION = "3.2.0";
+    public static final String VERSION = "3.3.0";
     /** stone block replacements that are Sedimentary */
     public static final List<Block> sedimentaryStones = new ArrayList<Block>();
     /** stone block replacements that are Metamorphic */

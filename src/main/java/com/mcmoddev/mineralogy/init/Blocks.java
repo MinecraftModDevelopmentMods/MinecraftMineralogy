19c1867f4c93c5a63e45aa3103cd384d35f30096
SkyBlade1978 <JohnBraham1978@outlook.com>
2019-09-06 20:34:45 +0200

    some more work on walls

--- src/main/java/com/mcmoddev/mineralogy/init/Blocks.java
+++ src/main/java/com/mcmoddev/mineralogy/init/Blocks.java
@@ -30,6 +30,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.block.WallBlock;
 import net.minecraft.block.material.Material;
 //import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;

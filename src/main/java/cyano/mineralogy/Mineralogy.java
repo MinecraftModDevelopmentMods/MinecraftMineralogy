1957d346ffe06dda987ccffa931113e8ec6e6e96
SkyBlade1978 <JohnBraham1978@outlook.com>
2019-09-13 09:49:35 +0100

    revisions to rocksaltlamp and lang files

--- src/main/java/cyano/mineralogy/Mineralogy.java
+++ src/main/java/cyano/mineralogy/Mineralogy.java
@@ -46,7 +46,7 @@ import java.util.*;
 public class Mineralogy {
 	public static final String MODID = "mineralogy";
     public static final String NAME ="Mineralogy";
-    public static final String VERSION = "3.3.2";
+    public static final String VERSION = "3.3.4";
     
     public static CreativeTabs mineralogyTab = new CreativeTabs("mineralogyTab"){
 		@Override

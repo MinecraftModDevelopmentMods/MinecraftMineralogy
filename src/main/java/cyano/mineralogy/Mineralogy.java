96d3eda97cfc5405ac7851fa17f184fde7003942
SkyBlade1978 <JohnBraham1978@outlook.com>
2019-09-18 15:19:24 +0100

    an attempt to fix the travis build

--- src/main/java/cyano/mineralogy/Mineralogy.java
+++ src/main/java/cyano/mineralogy/Mineralogy.java
@@ -46,7 +46,7 @@ import java.util.*;
 public class Mineralogy {
 	public static final String MODID = "mineralogy";
     public static final String NAME ="Mineralogy";
-    public static final String VERSION = "3.3.7";
+    public static final String VERSION = "3.3.8";
     
     public static CreativeTabs mineralogyTab = new CreativeTabs("mineralogyTab"){
 		@Override

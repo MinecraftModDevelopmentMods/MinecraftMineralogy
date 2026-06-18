db6596e6ceef65c07b08e8f81809fe6712e7bc0c
JohnBraham <johnbraham1978@outlook.com>
2026-06-18 09:09:47 +0100

    version bump

--- src/main/java/com/mcmoddev/mineralogy/Mineralogy.java
+++ src/main/java/com/mcmoddev/mineralogy/Mineralogy.java
@@ -22,7 +22,7 @@ public class Mineralogy {
 
 	public static final String MODID = "mineralogy";
 	public static final String NAME = "Mineralogy";
-	public static final String VERSION = "4.0.0.0";
+	public static final String VERSION = "4.1.0.0";
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
